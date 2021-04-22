f5fb8788009240c0ae0621b2eaa148335b3c66fb
Mahmoud Shehab <Mahmoud.Shehab@asset.com.eg>
2021-04-22 15:28:23 +0200

    Superflex

---
+++ CCAT_4_2_0/SCRIPT.sql
@@ -0,0 +1,7 @@
+INSERT INTO "ADM_SYSTEM_PROPERTIES" (GROUP_ID, ITEM_ID, GROUP_NAME, ITEM_VALUE, ENTRY_KEY, DESCRIPTION) 
+VALUES ('58',
+ '9',
+ 'SUPER_FLEX',
+ 'http://10.0.20.16:8081/ccat-simulators/CIGetPrepaidProfile',
+ 'SUPER_FLEX_THRESHOLD_GET_PRE_PROFILE', 
+ 'new url that retreives thrshold data at MI Threshold tab at super flex view page');
