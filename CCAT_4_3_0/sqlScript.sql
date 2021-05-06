d32852ce36a4f498ca533f927b45d7b1477e7ac8
Mahmoud Shehab <Mahmoud.Shehab@asset.com.eg>
2021-05-06 15:38:26 +0200

    4_3_0

---
+++ CCAT_4_3_0/sqlScript.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO "ADM_SYSTEM_PROPERTIES" (GROUP_ID, ITEM_ID, GROUP_NAME, ITEM_VALUE, ENTRY_KEY, DESCRIPTION) VALUES ('30', '42', 'ODS-PrePaid', 'Short message MO/PP,Mobile Telephony', 'BD_OP_DESTINATION_MASKED_KEYS', 'Balance Dispute keys if found should mask the other party coloumn');
+INSERT INTO "ADM_SYSTEM_PROPERTIES" (GROUP_ID, ITEM_ID, GROUP_NAME, ITEM_VALUE, ENTRY_KEY, DESCRIPTION) VALUES ('30', '43', 'ODS-PrePaid', 'Customer Services, IVR Services', 'BD_OP_ZONES_WHITELIST_KEYS', 'Balance Dispute keys if found shouldn''t mask the other party coloumn');
+INSERT INTO "ADM_SYSTEM_PROPERTIES" (GROUP_ID, ITEM_ID, GROUP_NAME, ITEM_VALUE, ENTRY_KEY, DESCRIPTION) VALUES ('30', '44', 'ODS-PrePaid', '5', 'BD_OP_DISPLAY_DIGITS', 'if 5, Should should view first 5 and masked the rest from column otherParty ');
+
